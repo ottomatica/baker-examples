ea2a2a36040af96ef85c92c87908f541924077b3
Chris Parnin <chris.parnin@gmail.com>
2018-08-24 07:08:42 -0400

    Inline baking with destroying (be less taxing)

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ for D in `find . -depth 1 -type d | grep -v .git`
 do
     echo "Destroying $D";
     (cd $D && baker destroy)
-done
-
-for D in `find . -depth 1 -type d | grep -v .git`
-do
     echo "Baking $D";
     (cd $D && baker bake)
 done
